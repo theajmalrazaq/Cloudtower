2c4a267fd208a606e2982f4ad06f34f12775aeb5
Ajmal Razzaq Bhatti <ajmalrazzaq07@gmail.com>
2023-12-07 01:02:10 +0500

    Add new assets and update script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+let btn_1 = document.getElementById('button_one');
+let btn_2 = document.getElementById('button_two');
+let btn_3 = document.getElementById('button_three');
+let con_1 = document.getElementById('con_one');
+let con_2 = document.getElementById('con_two');
+let con_3 = document.getElementById('con_three');
+function change1(){
+        btn_2.classList.remove('active');
+        btn_3.classList.remove('active');
+        con_1.style.display ="";
+        con_2.style.display ="none";
+        con_3.style.display="none";
+        btn_1.classList.add('active');
+
+    }
+    function change2(){
+        btn_1.classList.remove('active');
+        btn_3.classList.remove('active');
+        con_1.style.display ="none";
+        con_2.style.display ="";
+        con_3.style.display="none";
+        btn_2.classList.add('active');
+    }
+    function change3(){
+        btn_2.classList.remove('active');
+        btn_1.classList.remove('active');
+        con_1.style.display ="none";
+        con_2.style.display ="none";
+        con_3.style.display="";
+        btn_3.classList.add('active');
+    }
